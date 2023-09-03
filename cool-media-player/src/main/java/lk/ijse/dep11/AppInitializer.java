d1ad75d4135bda4aaecc6d2db9b0616f4fce7c32
Tharindu Nuwan Madhushanka <tharindunm94@gmail.com>
2023-09-03 19:29:39 +0530

    Create Scene

---
+++ cool-media-player/src/main/java/lk/ijse/dep11/AppInitializer.java
@@ -0,0 +1,27 @@
+package lk.ijse.dep11;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        BorderPane root = FXMLLoader.load(getClass().getResource("/view/MainScene.fxml"));
+        Scene mainScene = new Scene(root);
+        primaryStage.setScene(mainScene);
+        primaryStage.centerOnScreen();
+        primaryStage.setTitle("Cool Media Player");
+        primaryStage.show();
+
+    }
+}
---
+++ cool-media-player/src/main/java/lk/ijse/dep11/MainSceneController.java
@@ -0,0 +1,4 @@
+package lk.ijse.dep11;
+
+public class MainSceneController {
+}
