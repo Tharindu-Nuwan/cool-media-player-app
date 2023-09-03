ad0358a146be6973f3a3a01108cfbe17f43a665d
Tharindu Nuwan Madhushanka <tharindunm94@gmail.com>
2023-09-03 21:33:03 +0530

    Set actions for buttons

--- cool-media-player/src/main/java/lk/ijse/dep11/MainSceneController.java
+++ cool-media-player/src/main/java/lk/ijse/dep11/MainSceneController.java
@@ -1,8 +1,23 @@
 package lk.ijse.dep11;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.FileChooser;
+import javafx.util.Duration;
+
+import java.io.File;
 
 public class MainSceneController {
     public AnchorPane root;
@@ -10,16 +25,96 @@ public class MainSceneController {
     public Button btnPlay;
     public Button btnPause;
     public Button btnStop;
+    public MediaView mvMyVideo;
+    public Button btnSlow;
+    public Button btnFast;
+    public Button btnBackward;
+    public Button btnForward;
+    public Slider slrSeek;
+    public Slider slrVolume;
 
-    public void btnChooseFileOnACtion(ActionEvent actionEvent) {
+    private String path;
+    private MediaPlayer mediaPlayer;
+
+    public void btnChooseFileOnAction(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showOpenDialog(null);
+        path = file.toURI().toString();
+
+        if (path != null) {
+            Media media = new Media(path);
+            mediaPlayer = new MediaPlayer(media);
+            mvMyVideo.setMediaPlayer(mediaPlayer);
+            DoubleProperty widthProperty = mvMyVideo.fitWidthProperty();
+            DoubleProperty heightProperty = mvMyVideo.fitHeightProperty();
+            widthProperty.bind(Bindings.selectDouble(mvMyVideo.sceneProperty(), "width"));
+            heightProperty.bind(Bindings.selectDouble(mvMyVideo.sceneProperty(), "height"));
+
+            mediaPlayer.currentTimeProperty().addListener(new ChangeListener<Duration>() {
+                @Override
+                public void changed(ObservableValue<? extends Duration> observableValue, Duration duration, Duration t1) {
+                    slrSeek.setValue(t1.toSeconds());
+                }
+            });
+
+            mediaPlayer.setOnReady(new Runnable() {
+                @Override
+                public void run() {
+                    Duration totalDuration = mediaPlayer.getTotalDuration();
+                    slrSeek.setMax(totalDuration.toSeconds());
+                }
+            });
+
+            slrVolume.setValue(mediaPlayer.getVolume() * 100);
+            slrVolume.valueProperty().addListener(new InvalidationListener() {
+                @Override
+                public void invalidated(Observable observable) {
+                    mediaPlayer.setVolume(slrVolume.getValue() / 100);
+                }
+            });
+        }
     }
 
     public void btnPlayOnAction(ActionEvent actionEvent) {
+        mediaPlayer.play();
+        mediaPlayer.setRate(1.0);
     }
 
     public void btnPauseOnAction(ActionEvent actionEvent) {
+        mediaPlayer.pause();
     }
 
     public void btnStopOnAction(ActionEvent actionEvent) {
+        mediaPlayer.stop();
+    }
+
+    public void btnSlowOnAction(ActionEvent actionEvent) {
+        mediaPlayer.setRate(0.5);
+    }
+
+    public void btnFastOnAction(ActionEvent actionEvent) {
+        mediaPlayer.setRate(2.0);
+    }
+
+    public void btnForwardOnAction(ActionEvent actionEvent) {
+        mediaPlayer.seek(mediaPlayer.getCurrentTime().add(Duration.seconds(10)));
+    }
+
+    public void btnBackwardOnAction(ActionEvent actionEvent) {
+        mediaPlayer.seek(mediaPlayer.getCurrentTime().add(Duration.seconds(-10)));
+    }
+
+    public void slrSeekOnMousePressed(MouseEvent mouseEvent) {
+        mediaPlayer.seek(Duration.seconds(slrSeek.getValue()));
+    }
+
+    public void slrSeekOnMouseDragged(MouseEvent mouseEvent) {
+        mediaPlayer.seek(Duration.seconds(slrSeek.getValue()));
+    }
+
+    public void slrVolumeOnMouseDragged(MouseEvent mouseEvent) {
+    }
+
+    public void slrVolumeOnMousePressed(MouseEvent mouseEvent) {
     }
 }
